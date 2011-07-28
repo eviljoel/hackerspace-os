cee11a52cf8fc80c0989f156e8b64afeb9290650
Florian Hahn <flo@fhahn.com>
2011-07-28 21:38:34 +0200

    initial commit

---
+++ mos/cal/views.py
@@ -0,0 +1,104 @@
+# Create your views here.
+from dateutil.parser import *
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, HttpResponseRedirect
+
+from mos.cal.forms import EventForm
+from mos.cal.models import Event, Category, Location
+
+
+def display_special_events(request, typ, name):
+    """
+    Displays special events by location or category
+    """
+
+    try:
+        if typ == 'Category':
+            events = Event.objects.filter(category__name=name)
+            des = get_object_or_404(Category, name=name)
+        elif typ == 'Location':
+            events = Event.objects.filter(location__name=name)
+            des = get_object_or_404(Location, name=name)
+        else:
+            events = None
+            des = None
+
+    except ObjectDoesNotExist:
+        events = None
+
+    return render_to_response('cal/event_archive.html', {
+                               'latestevents': events,
+                               'title': name,
+                               'type': typ,
+                               'description': des,
+                               }, context_instance=RequestContext(request))
+
+
+@login_required
+def delete_event(request, object_id=None):
+    if not request.method == 'POST' or not request.user.is_authenticated():
+        return
+
+    event = Event.all.get(id=object_id)
+
+    event.delete()
+    event.save()
+
+    return HttpResponse()
+
+           
+@login_required
+def update_event(request, new, object_id=None):
+    if not request.POST or not request.user.is_authenticated():
+        #return
+        pass
+
+    if not new:
+        event = Event.all.get(id=object_id)
+    else:
+        event = Event()
+
+    event_error_id = None
+
+    event_valid = True
+
+    if request.method == 'POST':
+        event_form = EventForm(request.POST, instance=event)
+
+        if event_form.is_valid():
+            event_data = event_form.save(commit=False)
+            event_data.save(request.user, new)
+            event = Event.objects.get(id=event_data.id)
+        else:
+            event_valid = False
+            event_error_id = event.id
+
+    else:
+        event_form = EventForm()
+
+    response = render_to_response('cal/eventinfo_nf.inc', {
+            'event_has_error': not event_valid,
+            'event_form': event_form,
+            'event': event,
+            'new': not event.pk,
+            }, context_instance=RequestContext(request))
+
+    if not event_valid:
+        response.status_code = 500
+
+    return response
+
+
+def list(request, number=0):
+    events = Event.future.get_n(long(number) if number != '' else 0)
+
+    if not number:
+        events = events.reverse()
+
+    return render_to_response('cal/calendar.inc',
+                              {'latestevents': events},
+                              context_instance=RequestContext(request))
---
+++ mos/media/javascripts/ml-ajaxtools.js
@@ -0,0 +1,91 @@
+function submit_form(type, id){
+    myform = $(type + '-form-' + id);
+    new Ajax.Updater($(type + 'container' + id), myform.readAttribute('action'), {
+                  parameters: myform.serialize(true),
+    });
+}
+
+function delete_event(id) {
+    var frm = $('calendar-form-'+id);
+    var cnt = $('calendar-edit-'+id);
+
+    new Ajax.Request('/calendar/' + id + '/delete/', {
+	    onSuccess: function(r) {
+		new Ajax.Updater('calendar-content', calendarUpdateURL);
+	    } 
+    })
+}
+
+/*function delete_entry(type, id) {
+    var frm = $(type+'-form-'+id);
+    var cnt = $(type+'-edit-'+id);
+
+    alert(projectUpdateURL);
+    new Ajax.Request('/'+type+'/'+ id+'/delete/', {
+	    onSuccess: function(r) {
+		new Ajax.Updater(type+'-content', projectUpdateURL);
+	    } 
+    })
+    }*/
+
+function toggleView(type, id, onoff) {
+    view = $(type + '-view-' + id);
+    edit = $(type + '-edit-' + id);
+    
+    if (onoff) {
+        set_visible(edit);
+        set_invisible(view);
+    } else {
+        set_visible(view);
+        set_invisible(edit);    
+    }    
+    /*  Add 'Now' and 'Today' buttons to each field via javascript
+     *  Django normally does this onload but then it does not change dynamically added fields,
+     *  so we're doing this every time user clicks on 'Edit'
+     */
+    DateTimeShortcuts.init();
+}
+
+function set_visible(obj) {
+    obj.addClassName('visible');
+    obj.removeClassName('invisible');
+}
+
+function set_invisible(obj){
+    obj.addClassName('invisible');
+    obj.removeClassName('visible');
+}
+
+
+function do_on_load()
+{
+//    update_metasense();
+    DateTimeShortcuts.init();
+}
+
+function enter_pressed(e){
+    var keycode;
+    if (window.event) keycode = window.event.keyCode; 
+    else if (e) keycode = e.which; 
+    else return false; 
+    return (keycode == 13); 
+}
+
+addEvent(window, 'load', do_on_load);
+
+
+function submit_event(id) {
+    var frm = $('calendar-form-'+id);
+    var cnt = $('calendar-edit-'+id);
+
+    new Ajax.Request(frm.readAttribute('action'), {
+	    parameters: frm.serialize(true),
+		onSuccess: function(r) {
+		new Ajax.Updater('calendar-content', calendarUpdateURL);
+	    },
+		onFailure: function(r) {
+		cnt.innerHTML = r.responseText;
+		DateTimeShortcuts.init.defer(1);
+	    }
+    })
+}
